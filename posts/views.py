487b8815fadfd1b247e717062f4e5251e06a682e
Alexandr Nikonov <alex@sshaman.ru>
2017-09-03 17:22:58 +0300

    fetaute: add pagination

--- posts/views.py
+++ posts/views.py
@@ -1,6 +1,7 @@
 import datetime
 
 from django.conf import settings
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from django.http import JsonResponse
 from django.shortcuts import render, get_object_or_404
 
@@ -17,11 +18,20 @@ def get_base_ctx(page_title, page_name, page_summary):
 
 
 def index(request):
-    posts = models.Post.objects.order_by('-published_at').exclude(is_page=True).exclude(is_published=False).all()[
-            0:settings.POSTS_PER_PAGE]
+    posts = models.Post.objects.order_by('-published_at').exclude(is_page=True).exclude(is_published=False).all()
     ctx = get_base_ctx(settings.SITE_NAME, settings.SITE_NAME, settings.SITE_SUMMARY)
+
+    paginator = Paginator(posts, settings.POSTS_PER_PAGE)
+    page = request.GET.get('page')
+    try:
+        paged = paginator.page(page)
+    except PageNotAnInteger:
+        paged = paginator.page(1)
+    except EmptyPage:
+        paged = paginator.page(paginator.num_pages)
+
     ctx.update({
-        'posts': posts,
+        'posts':        paged,
         'is_main_page': True,
     })
     return render(request, 'posts/index.html', ctx)
