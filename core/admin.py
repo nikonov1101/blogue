c70e451a911c3c39490e3b23cfaa08dbecbc9ec4
Alexandr Nikonov <alex@sshaman.ru>
2018-03-19 21:23:22 +0300

    feature: customize post admin
    added:
    - Preview URL link on Post form.
    
    changed:
    - Group form inputs to make it more compact and pretty.
    - Disable actions on top for Post model.

--- core/admin.py
+++ core/admin.py
@@ -1,16 +1,37 @@
+from django.utils.translation import ugettext as _
 from django.contrib import admin
+from django.utils.safestring import mark_safe
 
 from core import models as core_models
 
 
-class PostAdmin(admin.ModelAdmin):
+class BaseAdmin(admin.ModelAdmin):
+    # disable selectable actions
+    actions_on_top = False
+    actions_on_bottom = False
+
+
+class PostAdmin(BaseAdmin):
     list_display = ('id', 'title', 'published_at', 'is_published', 'is_page', 'feed')
-    fields = ('title', 'summary', 'body', 'url_slug', 'published_at', 'head_image', 'is_published',
-              'is_page', 'comments_disabled', 'created_at', 'uuid', 'lang', 'feed')
-    readonly_fields = ('created_at', 'uuid')
+    readonly_fields = ('preview_url',)
+    fields = (
+        ('url_slug', 'preview_url'),
+        ('title', 'summary', 'feed'),
+        'body', 'published_at', 'head_image',
+        ('is_published', 'is_page', 'comments_disabled',),
+    )
+
+    def preview_url(self, obj=None):
+        if obj.pk:
+            return mark_safe("<a target='_blank' href='/preview/{}'>Preview</a>".format(obj.uuid))
+        else:
+            return _('Save the post first!')
+
+    preview_url.allow_tags = True
+    preview_url.short_description = _('Post preview')
 
 
-class PostImageAdmin(admin.ModelAdmin):
+class PostImageAdmin(BaseAdmin):
     pass
 
 
